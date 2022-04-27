e4badafb8f31095f0e792634117f0c785baa7ce9
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-04-27 15:37:02 -0500

    feedback 27/04/22 - fixed navbar logo position

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -32,12 +32,12 @@ const Navbar = () => {
 
     <nav className="fixed top-0 z-40 bg-NavBar w-full flex flex-col justify-center items-center py-6 md:py-10">
 
-      <div className="max-w-[1960px] w-full pr-4 md:pr-10 lg:pr-20 flex items-center justify-end">
+      <div className="max-w-[1440px] w-full pr-4 md:pr-10 lg:pr-20 flex items-center justify-end">
 
         {/* <div className="max-w-max space-x-[51px] md:space-x-0 md:max-w-[431px] lg:max-w-[700px] w-full flex items-center justify-between"> */}
         <div className="w-full flex items-center">
 
-          <div className="w-[calc(50vw+52px-16px)] md:w-[calc(50vw+52px-40px)] lg:w-[calc(50vw+52px-80px)] max-w-[calc(765px-80px)] flex items-center justify-between ml-auto">
+          <div className="w-[calc(50vw+52px-16px)] md:w-[calc(50vw+52px-40px)] lg:w-[calc(50vw+52px-80px)] max-w-[calc(769px-80px)] flex items-center justify-between ml-auto">
 
             <a 
               className="block w-max max-w-[96px] md:max-w-full max-h-[40px]" 

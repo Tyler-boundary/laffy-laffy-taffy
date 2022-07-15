10e1632e69239cd85d8df2bf1db642e50f5a0db6
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-07-14 21:20:06 -0500

    remove other things

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,11 +9,11 @@ import { assignColors } from "../helpers";
 import Contact from "../components/Contact";
 import Main from "../components/Main";
 
-const Home = ({featuredProduct,products,gallery}) => {
+const Home = ({gallery}) => {
 
   useEffect(() => {
     assignColors();
-  }, [products]);
+  }, []);
 
   return (
     <>
@@ -33,121 +33,9 @@ const Home = ({featuredProduct,products,gallery}) => {
 };
 
 export const getServerSideProps = async () => {
-
-  const endpoint = process.env.NEXT_PUBLIC_API_KEY;
-
-  async function featuredProduct(){
-
-    try {
-
-      const res = await fetch(endpoint.toString().concat(`/${process.env.NEXT_PUBLIC_MIDSECTION_FEATURED_PRODUCT ?? "2096389b-aa71-4f03-9cd0-242d6050e964" }`));
-      const response = await res.json();
-      const {error} = response;
-      if(error){
-        return {};
-      }else{
-        const {product:featured_product} = response;
-        if(featured_product){
-          return featured_product;
-        }
-
-        return response;
-        
-      }
-
-    } catch (error) {
-      return {};
-    }
-
-
-
-  }
-
-  async function x(){
-
-    const res = await fetch(endpoint+"?all=true&limit=100&offset=0");
-    const data = await res.json()
-    const {products} = data;
-    return products;
-    
-  }
-
-  async function requestProduct(id){
-
-    try {
-
-      if (!String.prototype.replaceAll) {
-        String.prototype.replaceAll = function(str, newStr){
-      
-          // If a regex pattern
-          if (Object.prototype.toString.call(str).toLowerCase() === '[object regexp]') {
-            return this.replace(str, newStr);
-          }
-      
-          // If a string
-          return this.replace(new RegExp(str, 'g'), newStr);
-      
-        };
-      }
-
-      // example ID = 5f267ee7-aaa1-4f7d-b9cf-776cdafe71db -> 844412 (each number is string.length of the split)
-
-      const productId = id.toString().trim().replaceAll("'","").replaceAll('"',"");
-
-      const keys = productId.split("-");
-      let validCombination = "";
-
-      keys.forEach(key => validCombination += `${key.length}` )
-
-      if(validCombination === "844412"){
-
-        const request = await fetch(endpoint.concat(`/${productId}`));
-        const response = await request.json();
-        const {error} = response;
-
-        if(error){
-          return {id};
-        }
-        
-        if(response.product){
-          return response.product;      
-        }
-
-        return response;
-        
-      }
-
-      return {id};
-
-    } catch (error) {
-      return {id};
-    }
-    
-  }
-
-  async function specificProducts(){
-
-    let arrayProducts =  process.env.NEXT_PUBLIC_ARRAY_IDS.split(",");
-    
-    try {
-      const allResponses = await Promise.all(arrayProducts.map(id => requestProduct(id)));  
-      return allResponses;
-    } catch (error) {
-      return {}
-    }
-    
-    
-  }
-
-  const [featured_product,products] = await Promise.all([featuredProduct(),specificProducts()]);
-
-  const filenames = fs.readdirSync('public/laffytaffy')
-  console.log(filenames);
-  
+  const filenames = fs.readdirSync('public/laffytaffy')  
   return {
     props: {
-      featuredProduct: featured_product,
-      products,
       gallery: filenames
     },
   };
